bcfc442d5acb60f1daaafdc217ada02905a30c06
IgnacySwiatlowski <ignacyswiatlowski@gmail.com>
2025-10-23 14:22:53 +0200

    generating by using sys.argv and deploying website

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
@@ -7,8 +8,10 @@ from gencontent import generate_pages_recursive
 dir_path_static = "./static"
 dir_path_public = "./public"
 dir_path_content = "./content"
+dir_path_docs = "./docs"
 template_path = "./template.html"
 
+basepath = sys.argv[2] if len(sys.argv) > 2 else "/"
 
 def main():
     print("Deleting public directory...")
@@ -16,10 +19,10 @@ def main():
         shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
-    copy_files_recursive(dir_path_static, dir_path_public)
+    copy_files_recursive(dir_path_static, dir_path_docs)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
 
 
 main()
